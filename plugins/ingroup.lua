db89e3497118674d5a5f215f3f214cbc64a63fad
Arash alone boy <titanteam0098@gmail.com>
2016-04-05 11:46:55 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -204,7 +204,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "Group settings:\nLock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nLock group leave : "..leave_ban.."\nflood sensitivity : "..NUM_MSG_MAX.."\nBot protection : "..bots_protection--"\nPublic: "..public
+  local text = "#Group settings:\n>>Lock group name : "..settings.lock_name.."\n>>Lock group photo : "..settings.lock_photo.."\n>>Lock group member : "..settings.lock_member.."\n>>Lock group leave : "..leave_ban.."\n>>flood sensitivity : "..NUM_MSG_MAX.."\n>>Bot protection : "..bots_protection--"\nPublic: "..public
   return text
 end
 
